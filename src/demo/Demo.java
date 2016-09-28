feda891e1d0d9ea0abc0d0b5ae81960c70106675
hanqf <qunfeng_han@126.com>
2016-09-28 18:14:45 +0800

    test

---
+++ src/demo/Demo.java
@@ -0,0 +1,31 @@
+/*
+ * COPYRIGHT Beijing NetQin-Tech Co.,Ltd.                                   *
+ ****************************************************************************
+ * 源文件名:  demo.Demo.java 													       
+ * 功能: cpframework框架													   
+ * 版本:	@version 1.0	                                                                   
+ * 编制日期: 2016年9月28日 下午6:08:07 						    						                                        
+ * 修改历史: (主要历史变动原因及说明)		
+ * YYYY-MM-DD |    Author      |	 Change Description		      
+ * 2016年9月28日    |    Administrator     |     Created 
+ */
+package demo;
+/** 
+ *Description: <类功能描述>. <br>
+ *<p>
+	<使用说明>
+ </p>
+ *Makedate:2016年9月28日 下午6:08:07 
+ * @author Administrator  
+ * @version V1.0                             
+ */
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+
